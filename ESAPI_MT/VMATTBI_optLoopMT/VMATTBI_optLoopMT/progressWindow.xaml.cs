ee68f3d33e0adbe86a704d89a78f024822b9b172
esimiele <simiele77@gmail.com>
2022-04-15 08:12:39 -0400

    Pull request #1

--- ESAPI_MT/VMATTBI_optLoopMT/VMATTBI_optLoopMT/progressWindow.xaml.cs
+++ ESAPI_MT/VMATTBI_optLoopMT/VMATTBI_optLoopMT/progressWindow.xaml.cs
@@ -148,7 +148,9 @@ namespace VMATTBI_optLoop
             //execute the optimization loop on the daughter thread
             slave.DoWork(d =>
             {
+                // TODO: I think this is where I have to add the query and results for feature requested in esimiele/VMAT-TBI#5
                 int percentCompletion = 0;
+                string currentOptimizerStatus = "";
                 //this dispatcher is for the daughter thread and tells the code to execute the below thread on the dauther thread asynchronously.
                 //These commands in this class are used to update the progress window UI (under control of the daughter thread)
                 Dispatcher.BeginInvoke((Action)(() => { abortStatus.Text = "Running"; }));
